e6a29c5fc8e6dedeb8f1c52ec1e759ea603e1be2
Gustavo Francelino <gustavo.francelino@gmail.com>
2021-12-20 20:15:58 +0000

    Add validators.

---
+++ Kalendario.Application/Validators/GetCustomersRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Kalendario.Application.Queries;
+
+namespace Kalendario.Application.Validators
+{
+    public class GetCustomersRequestValidator : AbstractValidator<GetEmployeesRequest>
+    {
+        public GetCustomersRequestValidator()
+        {
+            RuleFor(r => r.Search).MaximumLength(120).NotEmpty();
+            RuleFor(r => r.Length).LessThanOrEqualTo(150).NotEmpty();
+        }
+    }
+}
---
+++ Kalendario.Application/Validators/GetEmployeesRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Kalendario.Application.Queries;
+
+namespace Kalendario.Application.Validators
+{
+    public class GetEmployeesRequestValidator : AbstractValidator<GetEmployeesRequest>
+    {
+        public GetEmployeesRequestValidator()
+        {
+            RuleFor(r => r.Search).MaximumLength(120).NotEmpty();
+            RuleFor(r => r.Length).LessThanOrEqualTo(150).NotEmpty();
+        }
+    }
+}
