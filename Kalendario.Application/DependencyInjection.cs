0e319f49e39472166757a3ba06e7a93bad10971b
Gustavo Francelino <gustavo.francelino@gmail.com>
2021-12-21 12:09:09 +0000

    Add DependencyInjection in Kalendario.Application

---
+++ Kalendario.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Kalendario.Application.Common.Behaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kalendario.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
