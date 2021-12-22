343b3f42c5978063ea572e0ed010b227f6a995bb
Gustavo Francelino <gustavo.francelino@gmail.com>
2021-12-22 20:04:45 +0000

    add required services.

---
+++ Kalendario.Api/Services/CurrentUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Kalendario.Application.Common.Interfaces;
+
+namespace Kalendario.Api.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public CurrentUserService()
+        {
+            
+        }
+        public Guid UserId { get; }
+        public Guid AccountId { get; }
+        public bool IsAuthenticated { get; }
+    }
+}
