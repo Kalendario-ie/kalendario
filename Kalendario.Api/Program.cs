6baf38128da3eb988ce9d7e4a850cbcd7eed4ff0
Gustavo Francelino <gustavo.francelino@gmail.com>
2021-12-27 11:13:02 +0000

    Add Startup.cs

--- Kalendario.Api/Program.cs
+++ Kalendario.Api/Program.cs
@@ -1,77 +1,18 @@
-using FluentValidation.AspNetCore;
-using Kalendario.Api.Common;
-using Kalendario.Api.Services;
-using Kalendario.Application;
-using Kalendario.Application.Common.Interfaces;
-using Kalendario.Infrastructure;
-using Kalendario.Persistence;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Serialization;
+namespace Kalendario.Api;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true);
-
-builder.Services.AddKalendarioAuthentication(builder.Configuration)
-    .AddPersistence(builder.Configuration)
-    .AddApplication();
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddHealthChecks()
-    .AddDbContextCheck<KalendarioDbContext>();
-
-builder.Services.AddScoped<ICurrentUserService, CurrentUserService>()
-    .AddHttpContextAccessor();
-
-builder.Services.AddControllers();
-builder.Services.AddControllersWithViews()
-    .AddNewtonsoftJson(options =>
+public static class Program
+{
+    public static void Main(string[] args)
     {
-        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-    })
-    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<IKalendarioDbContext>());
-builder.Services.AddRazorPages();
+        var host = CreateHostBuilder(args).Build();
+        host.Run();
+    }
 
-builder.Services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
-    app.UseMigrationsEndPoint();
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddJsonFile("appsettings.Local.json", optional: true);
+            })
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
 }
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-
-app.UseCustomExceptionHandler();
-app.UseHealthChecks("/health");
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseIdentityServer();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-app.MapControllers();
-app.MapRazorPages();
-
-app.MapFallbackToFile("index.html"); ;
-
-app.Run();
---
+++ Kalendario.Api/Startup.cs
@@ -0,0 +1,97 @@
+﻿using FluentValidation.AspNetCore;
+using Kalendario.Api.Common;
+using Kalendario.Api.Services;
+using Kalendario.Application;
+using Kalendario.Application.Common.Interfaces;
+using Kalendario.Infrastructure;
+using Kalendario.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Serialization;
+
+namespace Kalendario.Api;
+
+public class Startup
+{
+    private IServiceCollection _services;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        Environment = environment;
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Environment { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+        services.AddKalendarioAuthentication(Configuration)
+            .AddPersistence(Configuration)
+            .AddApplication();
+
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddHealthChecks()
+            .AddDbContextCheck<KalendarioDbContext>();
+
+        services.AddScoped<ICurrentUserService, CurrentUserService>()
+            .AddHttpContextAccessor();
+
+        services.AddControllers();
+        services.AddControllersWithViews()
+            .AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            })
+            .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<IKalendarioDbContext>());
+        services.AddRazorPages();
+
+        services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app)
+    {
+        // Configure the HTTP request pipeline.
+        if (Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage();
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+
+        app.UseCustomExceptionHandler();
+        app.UseHealthChecks("/health");
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseIdentityServer();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action=Index}/{id?}");
+            endpoints.MapControllers();
+            endpoints.MapRazorPages();
+
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
