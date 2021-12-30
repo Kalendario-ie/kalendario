4f9c0e4547f5d9e61c5962db4a7712e1b7b8a7f2
Gustavo Francelino <gustavo.francelino@gmail.com>
2021-12-30 10:56:26 +0000

    Update AuthorizationHelper to get roles from entity.

--- Kalendario.Application/Authorization/AuthorizationHelper.cs
+++ Kalendario.Application/Authorization/AuthorizationHelper.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Kalendario.Application.Common.Interfaces;
+using Kalendario.Core.Domain;
 
 namespace Kalendario.Application.Authorization;
 
@@ -8,12 +11,25 @@ public static class AuthorizationHelper
 {
     public static List<string> GetAllRoles()
     {
-        var roleTypes = new List<string> {"Create", "Update", "Delete", "View"};
-
         return typeof(IKalendarioDbContext)
             .GetProperties()
-            .Select(p => p.PropertyType.GetGenericArguments()[0].Name)
-            .SelectMany(model => roleTypes.Select(role => $"{model}_{role}"))
+            .Select(p => p.PropertyType.GetGenericArguments()[0])
+            .SelectMany(entityType => FindEntityRoles(entityType).Select(role => $"{entityType.Name}_{role}"))
             .ToList();
+    }
+
+    private static IEnumerable<string> FindEntityRoles(Type type)
+    {
+        while (true)
+        {
+            var entityRolesMethod = type.GetMethod(nameof(BaseEntity.EntityRoles), BindingFlags.Public | BindingFlags.Static);
+
+            if (entityRolesMethod != null)
+            {
+                return (IEnumerable<string>) entityRolesMethod.Invoke(null, null);
+            }
+
+            type = type.BaseType;
+        }
     }
 }
--- Kalendario.Core/Domain/BaseEntity.cs
+++ Kalendario.Core/Domain/BaseEntity.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Kalendario.Core.Domain
+namespace Kalendario.Core.Domain;
+
+public class BaseEntity
 {
-    public class BaseEntity
-    {
-        public Guid Id { get; set; }
+    public Guid Id { get; set; }
 
-        public DateTime? DateCreated { get; set; }
-        
-        public Guid UserCreated { get; set; }
-        
-        public DateTime? DateModified { get; set; }
-        
-        public Guid UserModified { get; set; }
+    public DateTime? DateCreated { get; set; }
+
+    public Guid UserCreated { get; set; }
+
+    public DateTime? DateModified { get; set; }
+
+    public Guid UserModified { get; set; }
+
+    public const string CreateRole = "Create";
+
+    public const string UpdateRole = "Update";
+
+    public const string DeleteRole = "Delete";
+
+    public const string ViewRole = "View";
+
+    public static IEnumerable<string> EntityRoles()
+    {
+        return new List<string>() {CreateRole, UpdateRole, DeleteRole, ViewRole};
     }
+    
 }
