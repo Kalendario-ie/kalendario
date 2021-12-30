464cd86567ecea909f448216b7163b709382a6c8
Gustavo Francelino <gustavo.francelino@gmail.com>
2021-12-30 12:21:09 +0000

    Add unit tests to CreateAccountCommand

---
+++ Kalendario.Application.UnitTests/Commands/CreateAccountCommandUnitTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Kalendario.Application.Commands.Admin;
+using Kalendario.Application.Common.Exceptions;
+using Kalendario.Application.Common.Interfaces;
+using Kalendario.Application.UnitTests.Common;
+using Kalendario.Common;
+using Kalendario.Core.Domain;
+using Kalendario.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Mocks = Kalendario.Application.UnitTests.Common.Mocks;
+
+namespace Kalendario.Application.UnitTests.Commands;
+
+[TestClass]
+public class CreateAccountCommandUnitTests
+{
+    private Mock<ICurrentUserService> _currentUserServiceMock;
+    private Mock<IDateTime> _datetimeMock;
+    private KalendarioDbContext _context;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        this._currentUserServiceMock = Mocks.CurrentUserServiceMock();
+        this._datetimeMock = Mocks.DatetimeMock();
+        this._context = KalendarioContextFactory.Create(_currentUserServiceMock.Object, _datetimeMock.Object);
+    }
+
+    [TestMethod]
+    public async Task Should_CreateAccount()
+    {
+        var accountName = "Teste1";
+        var preAccounts = await this._context.Accounts.ToListAsync();
+
+        var createAccountCommand = new CreateAccountCommand {Name = accountName};
+        var result = await new CreateAccountCommand.Handler(_context)
+            .Handle(createAccountCommand, CancellationToken.None);
+
+        var postAccounts = await this._context.Accounts.ToListAsync();
+        Assert.AreEqual(preAccounts.Count + 1, postAccounts.Count);
+
+        var createdAccount = await _context.Accounts.FindAsync(result);
+
+        Assert.IsInstanceOfType(createdAccount, typeof(Account));
+        Assert.AreEqual(accountName, createdAccount.Name);
+    }
+
+    [TestMethod]
+    public async Task Should_ThrowError_OnRepeatedNames()
+    {
+        var accountName = "Teste1";
+
+        var createAccountCommand = new CreateAccountCommand {Name = accountName};
+        var handler = new CreateAccountCommand.Handler(_context);
+
+        var account1 = await handler
+            .Handle(createAccountCommand, CancellationToken.None);
+
+        var createAccountCommand2 = new CreateAccountCommand() {Name = accountName};
+        
+        await Assert.ThrowsExceptionAsync<ValidationException>(
+            () => handler.Handle(createAccountCommand2, CancellationToken.None));
+    }
+}
---
+++ Kalendario.Application/Commands/Admin/CreateAccountCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Kalendario.Application.Common.Exceptions;
+using Kalendario.Application.Common.Interfaces;
+using Kalendario.Application.Common.Security;
+using Kalendario.Core.Domain;
+using MediatR;
+
+namespace Kalendario.Application.Commands.Admin;
+
+[Authorize(typeof(Account), Account.CreateRole)]
+public class CreateAccountCommand : IKalendarioProtectedCommand<Guid>
+{
+    public string Name { get; set; }
+
+    public class Handler : IRequestHandler<CreateAccountCommand, Guid>
+    {
+        private readonly IKalendarioDbContext _context;
+
+        public Handler(IKalendarioDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Guid> Handle(CreateAccountCommand command, CancellationToken cancellationToken)
+        {
+            if (_context.Accounts.FirstOrDefault(a => a.Name == command.Name) != default)
+            {
+                throw new ValidationException()
+                {
+                    Errors = {[nameof(command.Name)] = new[] {"Name is already being used."}}
+                };
+            }
+
+            var account = new Account
+            {
+                Name = command.Name
+            };
+
+            await _context.Accounts.AddAsync(account, cancellationToken);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return account.Id;
+        }
+    }
+}
