f1db14a0427dbc0863fb388165e58c8920546e14
Gustavo Francelino <gustavo.francelino@gmail.com>
2022-01-05 16:41:33 +0000

    Add Schedule Entities.

---
+++ Kalendario.Core/Entities/Schedule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kalendario.Core.Entities;
+
+public class Schedule : AccountEntity
+{
+    public string Name { get; set; }
+
+    public List<ScheduleFrame> Frames { get; set; } = new();
+
+    public List<ScheduleFrame> Sunday  => FramesOf(DayOfWeek.Sunday);
+
+    public List<ScheduleFrame> Monday => FramesOf(DayOfWeek.Monday);
+    
+    public List<ScheduleFrame> Tuesday => FramesOf(DayOfWeek.Tuesday);
+
+    public List<ScheduleFrame> Wednesday => FramesOf(DayOfWeek.Wednesday);
+
+    public List<ScheduleFrame> Thursday => FramesOf(DayOfWeek.Thursday);
+
+    public List<ScheduleFrame> Friday => FramesOf(DayOfWeek.Friday);
+
+    public List<ScheduleFrame> Saturday => FramesOf(DayOfWeek.Saturday);
+
+    private List<ScheduleFrame> FramesOf(DayOfWeek dayOfWeek)
+    {
+        return Frames
+            .Where(f => f.Offset == dayOfWeek)
+            .OrderBy(f => f.Order)
+            .ToList();
+    }
+
+}
+
---
+++ Kalendario.Core/Entities/ScheduleFrame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kalendario.Core.Entities;
+
+public class ScheduleFrame : AccountEntity
+{
+    public DayOfWeek Offset { get; set; }
+
+    public int Order { get; set; }
+
+    public TimeOnly Start { get; set; }
+    
+    public TimeOnly End { get; set; }
+
+    public Guid ScheduleId { get; set; }
+
+    public Schedule Schedule { get; set; }
+}
