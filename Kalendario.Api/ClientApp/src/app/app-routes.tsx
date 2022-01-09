d3e2d36c875f437548ef9425dbed6e82b7ab415a
Gustavo Francelino <gustavo.francelino@gmail.com>
2022-01-09 18:31:11 +0000

    add new auth routes to app routes.

--- Kalendario.Api/ClientApp/src/app/app-routes.tsx
+++ Kalendario.Api/ClientApp/src/app/app-routes.tsx
@@ -1,15 +1,13 @@
-import EmployeeRoutes from 'src/app/modules/employee/employee-routes';
-import {EMPLOYEE_ROUTES} from 'src/app/modules/employee/urls';
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import AdminRoutes from 'src/app/modules/admin/admin-routes';
 import {ADMIN_ROUTES} from 'src/app/modules/admin/urls';
-import {AUTH_ROUTES} from 'src/app/modules/auth/urls';
+import EmployeeRoutes from 'src/app/modules/employee/employee-routes';
+import {EMPLOYEE_ROUTES} from 'src/app/modules/employee/urls';
 import {USER_ROUTES} from 'src/app/modules/users/urls';
 import UsersRoutes from 'src/app/modules/users/users-routes';
 import {ApplicationPaths} from 'src/components/api-authorization/ApiAuthorizationConstants';
 import ApiAuthorizationRoutes from 'src/components/api-authorization/ApiAuthorizationRoutes';
-import AuthContainer from './modules/auth/auth-container';
 import CompaniesRoutes from './modules/companies/companies-routes';
 import HomeContainer from './modules/core/home/home-container';
 import {ProtectedRoute} from './shared/util/router-extensions';
@@ -18,7 +16,6 @@ import {ProtectedRoute} from './shared/util/router-extensions';
 const AppRoutes: React.FunctionComponent = () => {
     return (
         <Switch>
-            <Route path={AUTH_ROUTES.ROOT} component={AuthContainer}/>
             <Route path="/c" component={CompaniesRoutes}/>
             <ProtectedRoute path={USER_ROUTES.ROOT} component={UsersRoutes}/>
             <ProtectedRoute path={ADMIN_ROUTES.ROOT} component={AdminRoutes}/>
