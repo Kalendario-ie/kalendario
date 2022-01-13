7fcf64c1e38fdb6918cae90de854ad739a557d28
Gustavo Francelino <gustavo.francelino@gmail.com>
2022-01-13 19:37:48 +0000

    Add employees controller.

--- Kalendario.Api/Controllers/EmployeesController.cs
+++ Kalendario.Api/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
-﻿using Kalendario.Application.Queries.Admin;
+﻿using Kalendario.Application.Commands.Admin;
+using Kalendario.Application.Queries.Admin;
 using Kalendario.Application.ResourceModels.Admin;
 using Kalendario.Application.Results;
 using Microsoft.AspNetCore.Mvc;
@@ -12,5 +13,18 @@ public class EmployeesController : ApiControllerBase
     public async Task<ActionResult<GetAllResult<EmployeeAdminResourceModel>>> Get([FromQuery] GetEmployeesQuery query)
     {
         return await Mediator.Send(query);
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<EmployeeAdminResourceModel>> Create([FromBody] UpsertEmployeeCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<ActionResult<EmployeeAdminResourceModel>> Update(Guid id, [FromBody] UpsertEmployeeCommand command)
+    {
+        command.Id = id;
+        return await Mediator.Send(command);
     }
 }
