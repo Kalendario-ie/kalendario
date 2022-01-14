c5f72d776ee7303730a1e9fb50656ce736e833e2
Gustavo Francelino <gustavo.francelino@gmail.com>
2022-01-14 15:16:21 +0000

    add AppointmentsController

---
+++ Kalendario.Api/Controllers/AppointmentsController.cs
@@ -0,0 +1,30 @@
+﻿using Kalendario.Application.Commands.Admin;
+using Kalendario.Application.Queries.Admin;
+using Kalendario.Application.ResourceModels.Admin;
+using Kalendario.Application.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kalendario.Api.Controllers;
+
+[ApiController]
+public class AppointmentsController : ApiControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<GetAppointmentsResult>> Get([FromQuery] GetAppointmentsQuery query)
+    {
+        return await Mediator.Send(query);
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<AppointmentAdminResourceModel>> Create([FromBody] UpsertAppointmentCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<ActionResult<AppointmentAdminResourceModel>> Update(Guid id, [FromBody] UpsertAppointmentCommand command)
+    {
+        command.Id = id;
+        return await Mediator.Send(command);
+    }
+}
